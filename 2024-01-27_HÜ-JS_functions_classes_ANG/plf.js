d9004ced7db23d87034430107dff5bb870e0aaf7
silv-salma <KHA230102@spengergasse.at>
2024-02-01 11:36:16 +0100

    commit

---
+++ 2024-01-27_HÜ-JS_functions_classes_ANG/plf.js
@@ -0,0 +1,28 @@
+class Person {
+    constructor(name, gender, groesseM, gewichtKG) {
+        this.name= name
+        this.gender= gender
+        this.groesseM= groesseM
+        this.gewichtKG= gewichtKG
+    }
+    groesseCM(){
+        return this.groesseM*100
+    }
+    getBmi(){
+        return this.gewichtKG / Math.pow(this.groesseM, 2)
+    }
+    getGewichtType(){
+        if (this.getBmi >>> 25){ return 'Übergewicht'} else 
+        {return 'Untergewicht'} 
+    }
+    vorName(){
+
+    }
+    nachName(){
+
+    }
+}
+new Person('John Doe', 'm', 1.80, 80)
+new Person('Eva Schwerlich', 'f', 1.60, 120)
+new Person('Peter Leicht', 'm', 1.90, 50)
+export { Person };
