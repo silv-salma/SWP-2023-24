5a16f2f718498f0e22ecfcf579308a57c43a5325
silv-salma <KHA230102@spengergasse.at>
2024-05-17 21:20:05 +0200

    practice

---
+++ Üben/practice/third.js
@@ -0,0 +1,25 @@
+var x = 13;
+
+document.getElementById("xy1").innerHTML.HTML = x;
+
+x = x+2;
+
+document.getElementById("xy1").innerHTML.HTML = x;
+
+var y = "Hallo";
+
+document.getElementById("xy2").innerHTML.HTML = y;
+
+document.getElementById("xy3").innerHTML.HTML = typeof(x);
+
+var z = 12;
+
+var a = x + z;
+
+document.getElementById("xy4").innerHTML.HTML = typepf(a);
+
+var
+let
+const pi = 3.14;
+
+pi = pi +2;
