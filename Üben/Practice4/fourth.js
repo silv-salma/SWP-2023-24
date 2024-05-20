aae8b240d6c8c948b4aa88733fec579aea2ec21e
silv-salma <KHA230102@spengergasse.at>
2024-05-20 23:00:07 +0200

    practice

--- Üben/Practice4/fourth.js
+++ Üben/Practice4/fourth.js
@@ -2,4 +2,11 @@ function addieren (zahl1,zahl2){
     return zahl1+zahl2;
 }
 
-let summe = addieren(3,7);
+let summe = addieren(5,7);
+let summe2 = addieren(6,7);
+
+document.write(summe); //nur zu Testzwecken benutzen
+
+function myfunction(){
+    document.getElementById("id1").innerHTML="25";
+}
