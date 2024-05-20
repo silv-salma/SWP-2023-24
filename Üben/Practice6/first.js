0b8df8b8c964010ef44b83241a35bf3106de28c8
silv-salma <KHA230102@spengergasse.at>
2024-05-20 23:52:06 +0200

    practice

---
+++ Üben/Practice6/first.js
@@ -0,0 +1,16 @@
+// if else
+// else if
+
+let a = 3; // auf 6 ändern dann kommt eine Ausgabe
+let b = 5;
+
+if (a > b){
+    document.write(a + " ist größer als " + b);
+}
+else if(a==b){
+    document.write(a + " ist gleich " + b);
+}
+
+else{
+    document.write(a + " ist kleiner als " +b);
+}
---
+++ Üben/Practice6/second.js
@@ -0,0 +1,15 @@
+//switch
+
+let tag = "Dienstag"; 
+
+switch(tag){
+    case "Montag";
+       document.write("Wochenstart");
+       break;
+    case "Dienstag";
+       document.write("Aha");
+       break;
+
+    default:
+        document.write("Keine Wochentag");
+}
